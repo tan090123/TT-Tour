c26115156daadeb372d6abff254b5cf91f122168
tan090123 <mtan23102003@gmail.com>
2023-05-12 11:14:00 +0700

    add feature vue.

---
+++ vue_travel/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h3></h3>
+  <Header/>
+  <Footer/>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_travel/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+
+import "./scss/_reset.scss"
+
+createApp(App).mount('#app')
