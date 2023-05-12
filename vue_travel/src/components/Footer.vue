33d3cbbc407ffd724bc50bcfaaf1de7b1319e5d6
vminhquang <kenshinri.epu@gmail.com>
2023-05-12 13:53:53 +0700

    footer

--- vue_travel/src/components/Footer.vue
+++ vue_travel/src/components/Footer.vue
@@ -1,10 +1,21 @@
 <template>
     <footer class="footer">
         <h2>Footer</h2>
+        <div class="div">
+            <div>
+                <h4>Du lịch nước ngoài</h4>
+                <h4>Du lịch nước ngoài</h4>
+                <h4>Du lịch nước ngoài</h4>
+                <h4>Du lịch nước ngoài</h4>
+                <h4>Du lịch nước ngoài</h4>
+            </div>
+        </div>
     </footer>
 </template>
 
+
 <script>
+    import '@/scss/_footer.scss';
     export default {
         name: 'footer-component',
         
