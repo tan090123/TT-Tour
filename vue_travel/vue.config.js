ac0772aa12a55bd7ba4bd989623e6fa34d959052
tan090123 <mtan23102003@gmail.com>
2023-06-03 22:35:09 +0700

    test connect api.

--- vue_travel/vue.config.js
+++ vue_travel/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:44363',
+        changeOrigin: true
+      }
+    }
+  }
 })
