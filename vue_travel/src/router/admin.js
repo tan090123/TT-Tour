847ae96e83513124dd5124f6017cb06b7730f2ff
tan090123 <mtan23102003@gmail.com>
2023-06-06 22:21:38 +0700

    connect api.

--- vue_travel/src/router/admin.js
+++ vue_travel/src/router/admin.js
@@ -2,21 +2,33 @@ const admin = [
     { path: '/admin', redirect: '/admin/dashboard' },
     {
         path: '/admin',
-        name: 'HomeAdmin',
+        name: 'admin',
         component: () => import("@/layout/Admin.vue"),
-        meta: { title: 'Trang admin - Vietravel' },
+        meta: { title: 'Trang chủ admin - Vietravel' },
         children: [
             {
                 path: 'dashboard',
                 name: 'HomeAdmin',
                 component: () => import("@/pages/admin/AdminHome.vue"),
-                meta: { title: 'Trang admin - Vietravel' }
+                meta: { title: 'Trang chủ admin - Vietravel' }
             },
             {
                 path: 'user',
-                name: 'user',
+                name: 'user-admin',
                 component: () => import("@/pages/admin/User.vue"),
-                meta: { title: 'Thông tin - Vietravel ' }
+                meta: { title: 'Quản lý thông tin người dùng - Vietravel ' }
+            },
+            {
+                path: 'tour',
+                name: 'tour-admin',
+                component: () => import("@/pages/admin/Tour.vue"),
+                meta: { title: 'Quản lý danh sách Tour - Vietravel ' }
+            },
+            {
+                path: 'tour/create',
+                name: 'create-tour',
+                component: () => import("@/pages/admin/CreateTour.vue"),
+                meta: { title: 'Trang tạo tour - Vietravel ' }
             },
         ]
     }
