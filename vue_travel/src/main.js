c08f4c6660f31e3eca4bef2b489948f236d83b13
vminhquang <kenshinri.epu@gmail.com>
2023-06-07 16:16:47 +0700

    .

--- vue_travel/src/main.js
+++ vue_travel/src/main.js
@@ -8,6 +8,5 @@ window.axios = axios;
 import "@/assets/scss/_reset.scss";
 
 const app=createApp(App);
-app.config.globalProperties.URL='https://localhost:44363';
 app.use(router)
 app.mount('#app')
--- vue_travel/src/pages/admin/CreateTour.vue
+++ vue_travel/src/pages/admin/CreateTour.vue
@@ -68,6 +68,7 @@
     </div>
 </template>
 <script>
+
 export default {
 
 }
